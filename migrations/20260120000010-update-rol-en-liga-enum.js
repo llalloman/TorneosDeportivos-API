d159b2d9635bc348f3ab67b392d76518ac621e10
AUTOCONTI\wmolina <wmolina@autoconsa.com>
2026-01-20 19:53:06 -0500

    Fix enum migration to properly handle PostgreSQL enum type change

--- migrations/20260120000010-update-rol-en-liga-enum.js
+++ migrations/20260120000010-update-rol-en-liga-enum.js
@@ -16,31 +16,67 @@ module.exports = {
       WHERE rol_en_liga IN ('delegado', 'arbitro');
     `);
 
-    // Cambiar el tipo de la columna al nuevo enum
+    // Crear el nuevo tipo enum
     await queryInterface.sequelize.query(`
-      ALTER TABLE usuario_ligas 
-      DROP CONSTRAINT IF EXISTS usuario_ligas_rol_en_liga_check;
+      DO $$ BEGIN
+        CREATE TYPE enum_usuario_ligas_rol_en_liga_new AS ENUM ('admin_liga', 'operador', 'visualizador');
+      EXCEPTION
+        WHEN duplicate_object THEN null;
+      END $$;
     `);
 
-    await queryInterface.changeColumn('usuario_ligas', 'rol_en_liga', {
-      type: Sequelize.ENUM('admin_liga', 'operador', 'visualizador'),
-      allowNull: false,
-      defaultValue: 'visualizador'
-    });
+    // Cambiar la columna para usar el nuevo tipo enum temporalmente con USING
+    await queryInterface.sequelize.query(`
+      ALTER TABLE usuario_ligas 
+      ALTER COLUMN rol_en_liga TYPE enum_usuario_ligas_rol_en_liga_new 
+      USING rol_en_liga::text::enum_usuario_ligas_rol_en_liga_new;
+    `);
+
+    // Eliminar el tipo enum antiguo si existe
+    await queryInterface.sequelize.query(`
+      DROP TYPE IF EXISTS enum_usuario_ligas_rol_en_liga CASCADE;
+    `);
+
+    // Renombrar el nuevo tipo enum al nombre original
+    await queryInterface.sequelize.query(`
+      ALTER TYPE enum_usuario_ligas_rol_en_liga_new RENAME TO enum_usuario_ligas_rol_en_liga;
+    `);
 
     console.log('Enum rol_en_liga actualizado exitosamente');
   },
 
   down: async (queryInterface, Sequelize) => {
+    // Crear el tipo enum antiguo
     await queryInterface.sequelize.query(`
-      ALTER TABLE usuario_ligas 
-      DROP CONSTRAINT IF EXISTS usuario_ligas_rol_en_liga_check;
+      DO $$ BEGIN
+        CREATE TYPE enum_usuario_ligas_rol_en_liga_old AS ENUM ('admin_liga', 'delegado', 'arbitro', 'visualizador');
+      EXCEPTION
+        WHEN duplicate_object THEN null;
+      END $$;
     `);
 
-    await queryInterface.changeColumn('usuario_ligas', 'rol_en_liga', {
-      type: Sequelize.ENUM('admin_liga', 'delegado', 'arbitro', 'visualizador'),
-      allowNull: false,
-      defaultValue: 'visualizador'
-    });
+    // Cambiar valores de 'operador' a 'delegado' para rollback
+    await queryInterface.sequelize.query(`
+      UPDATE usuario_ligas 
+      SET rol_en_liga = 'delegado' 
+      WHERE rol_en_liga = 'operador';
+    `);
+
+    // Cambiar la columna al tipo antiguo
+    await queryInterface.sequelize.query(`
+      ALTER TABLE usuario_ligas 
+      ALTER COLUMN rol_en_liga TYPE enum_usuario_ligas_rol_en_liga_old 
+      USING rol_en_liga::text::enum_usuario_ligas_rol_en_liga_old;
+    `);
+
+    // Eliminar el tipo enum nuevo
+    await queryInterface.sequelize.query(`
+      DROP TYPE IF EXISTS enum_usuario_ligas_rol_en_liga CASCADE;
+    `);
+
+    // Renombrar el tipo antiguo al nombre original
+    await queryInterface.sequelize.query(`
+      ALTER TYPE enum_usuario_ligas_rol_en_liga_old RENAME TO enum_usuario_ligas_rol_en_liga;
+    `);
   }
 };
